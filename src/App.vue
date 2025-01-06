3cc95e33e0ce5f97b3154df6cf5086e86389abf8
Braden <baj6164@gmail.com>
2025-01-05 18:01:07 -0700

    disabled dark mode until I make a setting for it

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,19 @@ const authStore = useAuthStore();
 onMounted(() => {
   authStore.startPolling();
 });
-
-onBeforeMount(() => {
-  if (new Date().getHours() >= 18 || new Date().getHours() <= 6) {
-    theme.global.name.value = "dark"
-  } else {
-    theme.global.name.value = "light"
-  }
-})
-setInterval(() => {
-  if (new Date().getHours() >= 18 || new Date().getHours() <= 6) {
-    theme.global.name.value = "dark"
-  } else {
-    theme.global.name.value = "light"
-  }
-}, 1000)
+//
+// onBeforeMount(() => {
+//   if (new Date().getHours() >= 18 || new Date().getHours() <= 6) {
+//     theme.global.name.value = "dark"
+//   } else {
+//     theme.global.name.value = "light"
+//   }
+// })
+// setInterval(() => {
+//   if (new Date().getHours() >= 18 || new Date().getHours() <= 6) {
+//     theme.global.name.value = "dark"
+//   } else {
+//     theme.global.name.value = "light"
+//   }
+// }, 1000)
 </script>
