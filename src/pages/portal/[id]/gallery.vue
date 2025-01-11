ee4af4531f34139058226aaa08db75e84b91fc92
Braden <baj6164@gmail.com>
2025-01-11 08:24:22 -0700

    image gallery updated

--- src/pages/portal/[id]/gallery.vue
+++ src/pages/portal/[id]/gallery.vue
@@ -2,40 +2,44 @@
   <DefaultLayout>
     <v-container>
       <v-row>
+        <!--        <v-col-->
+
+
+        <!--          cols="1"-->
+        <!--        >-->
+        <!--          -->
+        <!--          <v-img-->
+
+        <!--            aspect-ratio="1"-->
+        <!--            class="bg-grey-lighten-2"-->
+        <!--            cover-->
+        <!--          >-->
+        <!--            <template #default>-->
+        <!--              <v-card-title>Get Featured?</v-card-title>-->
+        <!--              <v-card-text>-->
+        <!--                Send photos of your fun adventures to katrina@katalystranch.com or tag us on instagram!-->
+        <!--              </v-card-text>-->
+        <!--              <v-img-->
+        <!--                aspect-ratio="1"-->
+        <!--                max-height="175px"-->
+        <!--                src="https://i.imgur.com/LAf5YiV.png"-->
+        <!--              />-->
+        <!--            </template>-->
+        <!--          </v-img>-->
+        <!--        </v-col>-->
         <v-col
 
-
-          class="d-flex child-flex"
-          cols="4"
+          v-for="image in images"
+          :key="image"
+          cols="12"
+          lg="3"
+          md="4"
+          sm="6"
         >
           <v-img
-
-            aspect-ratio="1"
-            class="bg-grey-lighten-2"
-            cover
-          >
-            <template #default>
-              <v-card-title>Get Featured?</v-card-title>
-              <v-card-text>
-                Send photos of your fun adventures to katrina@katalystranch.com or tag us on instagram!
-              </v-card-text>
-              <v-img
-                aspect-ratio="1"
-                max-height="175px"
-                src="https://i.imgur.com/LAf5YiV.png"
-              />
-            </template>
-          </v-img>
-        </v-col>
-        <v-col
-          v-for="n in 9"
-          :key="n"
-          class="d-flex child-flex"
-          cols="4"
-        >
-          <v-img
-            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+            :key="image"
+            :lazy-src="image"
+            :src="image"
             aspect-ratio="1"
             class="bg-grey-lighten-2"
             cover
@@ -60,4 +64,15 @@
 </template>
 <script lang="ts" setup>
 import DefaultLayout from "@/layouts/defaultLayout.vue";
+
+const images = [
+  "https://i.imgur.com/LAf5YiV.png",
+  "https://i.imgur.com/dgkQvJf.jpeg",
+  "https://i.imgur.com/OCZOoow.jpeg",
+  "https://i.imgur.com/y7h4UsZ.jpeg",
+  "https://i.imgur.com/jWLrmuR.jpeg",
+  "https://i.imgur.com/09VpuGf.jpeg",
+  "https://i.imgur.com/G3wRb5s.jpeg"
+
+]
 </script>
